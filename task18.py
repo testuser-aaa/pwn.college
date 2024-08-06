a43f6543d4b83fc5ff8e4e2da6b26cd3f389f3be
testuser-aaa <93967440+testuser-aaa@users.noreply.github.com>
2024-08-06 16:05:01 +0500

    Update and rename task17.py to task18.py
    made humanreadable output $: flag= pwn.collegw{****}

--- task18.py
+++ task18.py
@@ -1,6 +1,6 @@
 import re
 from pwn import *
-
+#change flag_end to number of questions
 s='/challenge/run'
 p=process(s)
 lvs=[b' UC ',b' C ',b' S ',b' TS ']
@@ -10,19 +10,22 @@ cat=[b'NUC',b'NATO',b'ACE',b'UFO']
 #       1       2      3      4
 sub_cat=[]
 obj_cat=[]
+#flag of the ending
+flag_end=b'64'
 #главный цикл
-for i in range(21):
+for i in range(64):
     sub_cat.clear()
     obj_cat.clear()
     tmp=p.recvuntil(b'Q ')
     qwe=p.recvline()
     x=0
     read=qwe.find(action[0])
-    # READ==TRUE, if action=read!!!
+    if_flag=qwe.find(flag_end)
+#READ==TRUE, if action=read!!!
     if (read>0): act=0
     else: act=1
-# в переменной act хранится действие        
-    for x in range(3):
+#в переменной act хранится действие        
+    for x in range(4):
         sub_l=qwe.find(lvs[x])
         if (sub_l>0 and sub_l<50): 
                subject_level=x+1
@@ -40,17 +43,23 @@ for i in range(21):
     for y in range(4):
         obj_cat_pos=qwe.rfind(cat[y])
         if obj_cat_pos>80: obj_cat.append(y+1)
-        #получили массив из категорий объекта
+#получили массив из категорий объекта
     if_read=set(sub_cat)>=set(obj_cat)
     if_write=set(obj_cat)>=set(sub_cat)
     if act==0:
         if subject_level>=object_level and if_read: 
-           p.sendline(b'yes')
+           string_to_send=b'yes'
         else: 
-           p.sendline(b'no')
+           string_to_send=b'no'
     if (act==1):
         if object_level>=subject_level and if_write:
-           p.sendline(b'yes')
-        else: p.sendline(b'no')
+           string_to_send=b'yes'
+        else: string_to_send=b'no'
+    if if_flag>=0: 
+       p.sendline(string_to_send)
+       p.recvuntil(b'pwn')
+       flag=b'pwn'+p.recvuntil(b'}')
+    else: p.sendline(string_to_send)
 #=====================================
+print('flag= ',flag.decode())
 p.close()
